3adaf954f274731b1fad7ac08987c1621ee27030
Hanna <anna@example.com>
2019-10-23 21:00:15 +0300

    first commit

---
+++ app/Http/Controllers/MaintextController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MaintextController extends Controller
+{
+    public function getIndex ($url=null){
+		return view('static',compact('url'));
+	}
+}
